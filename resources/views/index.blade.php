bcbc514e898238b94b7ae128fb470821ed588ad3
Ahmad <ahmadbunni@gmail.com>
2023-02-03 01:19:08 +0300

    removing all of banner and offers section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,7 @@
             </div>
         </div>
         <!-- preloader end -->
+
         <!-- click effect -->
         <div class="sb-click-effect"></div>
         <!-- loader -->
@@ -240,29 +241,6 @@
         <!-- dynamic content -->
         <div id="sb-dynamic-content" class="sb-transition-fade">
 
-            <!-- banner -->
-            <section class="sb-banner sb-banner-xs sb-banner-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <!-- main title -->
-                            <div class="sb-main-title-frame">
-                                <div class="sb-main-title">
-                                    <h1 class="sb-h2">Starbelly menu</h1>
-                                    <ul class="sb-breadcrumbs">
-                                        <li><a href="home-1.html">Home</a></li>
-                                        <li><a href="#.">Menu</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <!-- main title end -->
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- banner end -->
-
-            <!-- menu section 1 -->
             <section class="sb-menu-section sb-p-90-60">
                 <div class="sb-bg-1">
                     <div></div>
@@ -1001,89 +979,6 @@
                 </div>
             </section>
             <!-- menu end -->
-
-            <!-- promo -->
-            <section class="sb-p-0-60">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="sb-promo-frame sb-mb-30">
-                                <div class="sb-promo-content">
-                                    <div class="sb-text-frame">
-                                        <h3 class="sb-mb-10"><sup>-</sup> <b class="sb-h2">50</b> <sup>%</sup></h3>
-                                        <h3 class="sb-mb-15">Discount for all* burgers!</h3>
-                                        <p class="sb-text sb-text-sm sb-mb-15">*Et modi itaque praesentium.</p>
-                                        <!-- button -->
-                                        <a href="product.html" class="sb-btn sb-ppc">
-                      <span class="sb-icon">
-                        <img src="img/ui/icons/arrow.svg" alt="icon">
-                      </span>
-                                            <span>Get it now</span>
-                                        </a>
-                                        <!-- button end -->
-                                    </div>
-                                    <div class="sb-image-frame">
-                                        <div class="sb-illustration-4">
-                                            <img src="img/illustrations/burger.png" alt="burger" class="sb-burger">
-                                            <div class="sb-cirkle-1"></div>
-                                            <div class="sb-cirkle-2"></div>
-                                            <div class="sb-cirkle-3"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="sb-promo-frame sb-mb-30">
-                                <div class="sb-promo-content">
-                                    <div class="sb-text-frame">
-                                        <h3 class="sb-mb-15">Visit Starbelly and get your coffee*</h3>
-                                        <h3 class="sb-mb-10"><b class="sb-h2">For free!</b></h3>
-                                        <p class="sb-text sb-text-sm sb-mb-15">*Et modi itaque praesentium.</p>
-                                        <!-- button -->
-                                        <a href="product.html" class="sb-btn sb-ppc">
-                      <span class="sb-icon">
-                        <img src="img/ui/icons/arrow.svg" alt="icon">
-                      </span>
-                                            <span>Get it now</span>
-                                        </a>
-                                        <!-- button end -->
-                                    </div>
-                                    <div class="sb-image-frame">
-                                        <div class="sb-illustration-5">
-                                            <img src="img/illustrations/cup.png" alt="cup" class="sb-cup">
-                                            <div class="sb-cirkle-1"></div>
-                                            <div class="sb-cirkle-2"></div>
-                                            <div class="sb-cirkle-3"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- promo end -->
-
-            <!-- footer -->
-            <footer>
-                <div class="container">
-                    <div class="sb-footer-frame">
-                        <a href="home-1.html" class="sb-logo-frame">
-                            <!-- logo img -->
-                            <img src="img/ui/logo.svg" alt="Starbelly">
-                        </a>
-                        <ul class="sb-social">
-                            <li><a href="#."><i class="far fa-circle"></i></a></li>
-                            <li><a href="#."><i class="far fa-circle"></i></a></li>
-                            <li><a href="#."><i class="far fa-circle"></i></a></li>
-                            <li><a href="#."><i class="far fa-circle"></i></a></li>
-                        </ul>
-                        <div class="sb-copy">&copy; late 2021 Starbelly. All Rights Reserved.</div>
-                    </div>
-                </div>
-            </footer>
-            <!-- footer end -->
 
         </div>
         <!-- dynamic content end -->
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,38 @@
-<!-- jquery js -->
-<script src="js/plugins/jquery.min.js"></script>
-<!-- smooth scroll js -->
-<script src="js/plugins/smooth-scroll.js"></script>
-<!-- swup js -->
-<script src="js/plugins/swup.min.js"></script>
-<!-- swiper js -->
-<script src="js/plugins/swiper.min.js"></script>
-<!-- datepicker js -->
-<script src="js/plugins/datepicker.js"></script>
-<!-- isotope js -->
-<script src="js/plugins/isotope.js"></script>
-<!-- sticky -->
-<script src="js/plugins/sticky.js"></script>
-<!-- mapbox js -->
-<script src="js/plugins/mapbox.min.js"></script>
-<!-- fancybox js -->
-<script src="js/plugins/fancybox.min.js"></script>
-<!-- starbelly js -->
-<script src="js/main.js"></script>
+<footer>
+    <div class="container">
+        <div class="sb-footer-frame">
+            <a href="home-1.html" class="sb-logo-frame">
+                <!-- logo img -->
+                <img src="img/ui/logo.svg" alt="Starbelly">
+            </a>
+            <ul class="sb-social">
+                <li><a href="#."><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#."><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#."><i class="fab fa-snapchat"></i></a></li>
+            </ul>
+            <div class="sb-copy">&copy; late 2023 more & more. All Rights Reserved.</div>
+        </div>
+    </div>
+
+    <!-- jquery js -->
+    <script src="js/plugins/jquery.min.js"></script>
+    <!-- smooth scroll js -->
+    <script src="js/plugins/smooth-scroll.js"></script>
+    <!-- swup js -->
+    <script src="js/plugins/swup.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/plugins/swiper.min.js"></script>
+    <!-- datepicker js -->
+    <script src="js/plugins/datepicker.js"></script>
+    <!-- isotope js -->
+    <script src="js/plugins/isotope.js"></script>
+    <!-- sticky -->
+    <script src="js/plugins/sticky.js"></script>
+    <!-- mapbox js -->
+    <script src="js/plugins/mapbox.min.js"></script>
+    <!-- fancybox js -->
+    <script src="js/plugins/fancybox.min.js"></script>
+    <!-- starbelly js -->
+    <script src="js/main.js"></script>
+
+</footer>
