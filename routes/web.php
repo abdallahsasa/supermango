9ba248d2f9ee5299582f6435931575712dbf54db
abdallahsasa <abdallah97sasa@gmail.com>
2023-02-05 01:06:45 +0300

    soon page default

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('index');
 });
-Route::get('/soon', function () {
+Route::get('/', function () {
     return view('coming_soon');
 });
 Route::get('/contact', function () {
