d07ce59a8949f1760ebd92decd33a88479f88318
abdallahsasa <abdallah97sasa@gmail.com>
2023-03-10 15:40:48 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <!-- starbelly css -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <!-- page title -->
-    <title>More & More</title>
+    <title>Juice World</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
