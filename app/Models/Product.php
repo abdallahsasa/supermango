c88c2b8158f4b907bead9f44891ee4c634d5739d
abdallahsasa <abdallah97sasa@gmail.com>
2023-10-04 01:03:12 +0300

    front 2

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,45 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
     protected $table = 'products';
-    protected $fillable = array('name','description','price','active','image','category_id','sku');
+    protected $fillable = array('name', 'description', 'price', 'active', 'image', 'category_id', 'sku');
 
     public function category()
     {
         return $this->belongsTo('App\Models\Category');
     }
+
     public function translations()
     {
         return $this->hasMany('App\Models\ProductTranslation');
     }
+
     public function getNameAttribute($attribute)
     {
-        if(session()->has('locale') && session('locale') != "en")
-        {
+        if (session()->has('locale') && session('locale') != "en") {
             return $this->get_name_trans(session('locale'))->name ?? $attribute;
         }
         return $attribute;
     }
+
     public function get_name_trans($lang)
     {
         return $this->translations()
-            ->where('lang','=',$lang)
+            ->where('lang', '=', $lang)
             ->first();
     }
+
     public function getDescriptionAttribute($attribute)
     {
-        if(session()->has('locale') && session('locale') != "en")
-        {
+        if (session()->has('locale') && session('locale') != "en") {
             return $this->get_description_trans(session('locale'))->description ?? $attribute;
         }
         return $attribute;
     }
+
     public function get_description_trans($lang)
     {
-        return  $this->translations()
-            ->where('lang','=',$lang)
+        return $this->translations()
+            ->where('lang', '=', $lang)
             ->first('description');
     }
+
     public function media()
     {
         return $this->hasMany('App\Models\ProductMedia');
@@ -56,15 +61,18 @@ class Product extends Model
     {
         return $this->hasMany('App\Models\ProductTag');
     }
+
     public function prices()
     {
         return $this->hasMany('App\Models\ProductPrices');
     }
+
     public function regularprice()
     {
-        $regularprice  = ProductPrices::where(['product_id ' => $this->getKey(),'size'=> 'Regular']);
+        $regularprice = ProductPrices::where(['product_id' => $this->getKey(), 'size' => 'Regular'])->pluck('price')->first();
         return $regularprice;
     }
+
     public function attributes()
     {
         return $this->belongsToMany('App\Models\Attribute')->withPivot('values');
@@ -82,7 +90,7 @@ class Product extends Model
 
     public static function getActiveProducts()
     {
-        return self::where(['status' => 'active','stock_status' => 'in'])->where('stock_quantity','>',0);
+        return self::where(['status' => 'active', 'stock_status' => 'in'])->where('stock_quantity', '>', 0);
     }
 
     public function in_stock()
--- resources/views/website/products/index.blade.php
+++ resources/views/website/products/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <!-- page title start -->
-    <section class="page_title_area" data-background="{{asset('website/img/bg/page_title3.png')}}" style="background-repeat: no-repeat;">
+    <section class="page_title_area" data-background="{{asset('website/img/bg/page_title3.png')}}"
+             style="background-repeat: no-repeat;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
@@ -29,43 +30,43 @@
                     <div class="masonry_active mb-40">
                         <button class="active" data-filter="*">all categories</button>
                         @foreach($categories as $category)
-                        <button data-filter=".{{$category->id}}">{{$category->name}}</button>
+                            <button data-filter=".{{$category->id}}">{{$category->name}}</button>
                         @endforeach
                     </div>
                 </div>
             </div>
             <div class="row grid">
                 @foreach($products as $product)
-                <div class="col-lg-4 col-md-6 col-sm-6 grid-item mb-30 {{$product->category->id}}">
-                    <div class="shop_single white_bg">
-                        <div class="thumb text-center">
-                            <a class="image" href=""><img src="{{$product->image_url}}" alt=""></a>
-                        </div>
-                        <div class="content">
-                            <div class="s_top ul_li">
-                                <span class="cat">{{$product->category->name}}</span>
-                                <ul class="rating_star ul_li">
-                                    <li><i class="fas fa-star"></i></li>
-                                    <li><i class="fas fa-star"></i></li>
-                                    <li><i class="fas fa-star"></i></li>
-                                    <li><i class="fal fa-star"></i></li>
-                                    <li><i class="fal fa-star"></i></li>
-                                </ul>
+                    <div class="col-lg-4 col-md-6 col-sm-6 grid-item mb-30 {{$product->category->id}}">
+                        <div class="shop_single white_bg">
+                            <div class="thumb text-center">
+                                <a class="image" href="{{route('website.products.details',$product->id)}}"><img src="{{$product->image_url}}" alt=""></a>
                             </div>
-                            <h3 class="title"><a href="shop-details.html">{{$product->name}}</a></h3>
-                            <div class="s_bottom ul_li">
-                                <span class="price">Price -</span>
-                                <span>$53.00</span>
+                            <div class="content">
+                                <div class="s_top ul_li">
+                                    <span class="cat">{{$product->category->name}}</span>
+                                    <ul class="rating_star ul_li">
+                                        <li><i class="fas fa-star"></i></li>
+                                        <li><i class="fas fa-star"></i></li>
+                                        <li><i class="fas fa-star"></i></li>
+                                        <li><i class="fal fa-star"></i></li>
+                                        <li><i class="fal fa-star"></i></li>
+                                    </ul>
+                                </div>
+                                <h3 class="title"><a href="{{route('website.products.details',$product->id)}}">{{$product->name}}</a></h3>
+                                <div class="s_bottom ul_li">
+                                    <span class="price">Price</span>
+                                    <span>{{$product->regularprice()}}</span>
+
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
 
         </div>
     </section>
     <!-- shop end -->
-
 
 @endsection
