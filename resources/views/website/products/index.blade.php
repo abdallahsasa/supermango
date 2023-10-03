a3fe467d14aa29fa0d0cbca8722a6e12d7d7cd9c
abdallahsasa <abdallah97sasa@gmail.com>
2023-10-04 00:52:35 +0300

    front 1

--- resources/views/website/products/index.blade.php
+++ resources/views/website/products/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- page title start -->
-    <section class="page_title_area" data-background="{{asset('website/img/bg/page_title3.png')}}">
+    <section class="page_title_area" data-background="{{asset('website/img/bg/page_title3.png')}}" style="background-repeat: no-repeat;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
@@ -22,7 +22,7 @@
     <!-- page title end -->
 
     <!-- shop start -->
-    <section class="shop_area pt-120 pb-120">
+    <section class="shop_area pt-40 pb-40">
         <div class="container">
             <div class="row">
                 <div class="col-12">
